788f19f98af54c4e9bec2008c3ecb26fc7bdabf3
Mila <mila@NDT-PC02.simetria.intranet>
2015-04-23 18:19:00 -0300

    novos asserts para testar cobertura

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -5,10 +5,16 @@ class CalculadoraTest extends PHPUnit_Framework_TestCase {
 
 	 public function testCalculadora() {
 
-	 	$calculadora = new Calculadora();
+	$calculadora = new Calculadora();
 
         $this->assertEquals(10, $calculadora->sumar(4, 6));
+        
+        $this->assertEquals(2, $calculadora->restar(6, 4));
 
+        $this->assertEquals(10, $calculadora->multiplicar(2, 5));
+        
+        $this->assertEquals(5, $calculadora->dividir(10, 2));
+        
         $this->assertEquals(10, $calculadora->multiplicar(2, 5));
 
         $this->assertEquals(24, $calculadora->factorial(4));
